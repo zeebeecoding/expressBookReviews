eed47c9ded1f52982ff41495620bc2f73ce97768
Aajzi-Pasand <20ntucs1139@student.ntu.edu.pk>
2023-09-24 22:51:42 +0500

    Project Completed

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,85 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  const token = req.session.authorization?.accessToken;
+
+  if (token) {
+    jwt.verify(token, "access", (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid token" });
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(403).json({ message: "User not authenticated" });
+  }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
+
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const session = require("express-session");
+// const customer_routes = require("./router/auth_users.js").authenticated;
+// const genl_routes = require("./router/general.js").general;
+
+// const app = express();
+
+// app.use(express.json());
+
+// app.use(
+//   "/customer",
+//   session({
+//     secret: "fingerprint_customer",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
+
+// app.use("/customer/auth/*", function auth(req, res, next) {
+//   const token = req.session.authorization?.accessToken;
+
+//   if (token) {
+//     jwt.verify(token, "access", (err, user) => {
+//       if (err) {
+//         return res.status(403).json({ message: "Invalid token" });
+//       }
+//       req.user = user;
+//       next();
+//     });
+//   } else {
+//     return res.status(403).json({ message: "User not authenticated" });
+//   }
+// });
+
+// const PORT = 5000;
+
+// app.use("/customer", customer_routes);
+// app.use("/", genl_routes);
+
+// app.listen(PORT, () =>
+//   console.log(`Server running at http://localhost:${PORT}`)
+// );
--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,79 @@
-let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+// let books = {
+//   1: {
+//     author: "Chinua Achebe",
+//     title: "Things Fall Apart",
+//     reviews: ["Excellent read!", "Highly recommended"],
+//   },
+//   2: {
+//     author: "Hans Christian Andersen",
+//     title: "Fairy tales",
+//     reviews: ["Magical and enchanting", "Timeless classics for all ages"],
+//   },
+//   3: {
+//     author: "Dante Alighieri",
+//     title: "The Divine Comedy",
+//     reviews: [
+//       "A masterpiece of literature",
+//       "Incredible journey through the afterlife",
+//     ],
+//   },
+//   4: {
+//     author: "Unknown",
+//     title: "The Epic Of Gilgamesh",
+//     reviews: ["Ancient epic with enduring themes"],
+//   },
+//   5: {
+//     author: "Unknown",
+//     title: "The Book Of Job",
+//     reviews: ["Deep philosophical reflections"],
+//   },
+//   6: {
+//     author: "Unknown",
+//     title: "One Thousand and One Nights",
+//     reviews: ["Tales of wonder and adventure"],
+//   },
+//   7: {
+//     author: "Unknown",
+//     title: "Nj\u00e1l's Saga",
+//     reviews: ["Epic saga of medieval Iceland"],
+//   },
+//   8: {
+//     author: "Jane Austen",
+//     title: "Pride and Prejudice",
+//     reviews: ["Classic romance novel", "Witty and insightful"],
+//   },
+//   9: {
+//     author: "Honor\u00e9 de Balzac",
+//     title: "Le P\u00e8re Goriot",
+//     reviews: ["A brilliant portrayal of society", "Compelling characters"],
+//   },
+//   10: {
+//     author: "Samuel Beckett",
+//     title: "Molloy, Malone Dies, The Unnamable, the trilogy",
+//     reviews: ["Existential masterpiece", "Experimental and thought-provoking"],
+//   },
+// };
 
-module.exports=books;
+// module.exports = books;
+let books = {
+  1: { author: "Chinua Achebe", title: "Things Fall Apart", reviews: {} },
+  2: { author: "Hans Christian Andersen", title: "Fairy tales", reviews: {} },
+  3: { author: "Dante Alighieri", title: "The Divine Comedy", reviews: {} },
+  4: { author: "Unknown", title: "The Epic Of Gilgamesh", reviews: {} },
+  5: { author: "Unknown", title: "The Book Of Job", reviews: {} },
+  6: { author: "Unknown", title: "One Thousand and One Nights", reviews: {} },
+  7: { author: "Unknown", title: "Nj\u00e1l's Saga", reviews: {} },
+  8: { author: "Jane Austen", title: "Pride and Prejudice", reviews: {} },
+  9: {
+    author: "Honor\u00e9 de Balzac",
+    title: "Le P\u00e8re Goriot",
+    reviews: {},
+  },
+  10: {
+    author: "Samuel Beckett",
+    title: "Molloy, Malone Dies, The Unnamable, the trilogy",
+    reviews: {},
+  },
+};
+
+module.exports = books;
